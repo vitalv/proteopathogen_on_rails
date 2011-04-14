58847205da87fb9660cadc9143c86cf2020fcb1d
Vital V <uveuve@gmail.com>
2011-04-14 22:54:16 +0200

    brand new proteopathogen on rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProteopathogenOnRails::Application.config.secret_token = 'bebad7503282352e0970dfc3340bb195c2b3b8c8241ee2f092a318e29c22b341a1fdbb73521a64e07b96c3784387b9cf5db1457f77b6965a68ffeff22df88ac5'
