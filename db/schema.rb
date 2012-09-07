f9b5e2627530029f5c197afbf720d6123b47a6d9
vitalv <uveuve@gmail.com>
2012-09-07 17:23:58 +0200

    run sil_migration, included to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120907133329) do
+ActiveRecord::Schema.define(:version => 20120907141137) do
+
+  create_table "spectrum_identification_lists", :force => true do |t|
+    t.string   "sil_id",                              :null => false
+    t.integer  "spectrum_identification_protocol_id"
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
+  end
 
   create_table "spectrum_identification_protocols", :force => true do |t|
     t.string   "sip_id",            :null => false
