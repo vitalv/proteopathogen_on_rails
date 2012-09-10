998fe9efba7e29f42fe716692a408cea07b15fd1
vitalv <uveuve@gmail.com>
2012-09-10 11:10:06 +0200

    migration to add threshold to sip table. Apparently threshold is mandatory

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120909154217) do
+ActiveRecord::Schema.define(:version => 20120910090202) do
 
   create_table "spectrum_identification_items", :force => true do |t|
     t.string   "sii_id",                            :null => false
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20120909154217) do
     t.string   "search_type"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.string   "threshold",         :null => false
   end
 
   create_table "spectrum_identification_results", :force => true do |t|
