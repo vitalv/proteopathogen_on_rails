996a3a1a60fc0bae91b4aac14911d4bfb94eac3d
Vital @ UCM <uveuve@gmail.com>
2012-09-10 16:41:09 +0200

    on branch ucm:dbthing . Run a migration to add pass_threshold to sii table, which apparently is mandatory

--- db/migrate/20120910101515_create_searched_modifications.rb
+++ db/migrate/20120910101515_create_searched_modifications.rb
@@ -3,11 +3,7 @@ class CreateSearchedModifications < ActiveRecord::Migration
     create_table :searched_modifications do |t|
       t.references :spectrum_identification_protocol, :null => false
       t.float :mass_delta
-<<<<<<< HEAD
       t.boolean :is_fixed
-=======
-      t.bool :is_fixed
->>>>>>> 37ba5eb901adecb733eb4e4cc88673082c7b4600
       t.string :residue
       t.timestamps
     end
