2b7062cc6cc45d65a487434a46389f721499effd
vitalv <uveuve@gmail.com>
2012-09-10 12:50:31 +0200

    on branch ucm/dbthing. Edited searched_modification model and migration that were created (by mistake) on master. Don't forget to run the migration

--- app/models/searched_modification.rb
+++ app/models/searched_modification.rb
@@ -1,3 +1,5 @@
 class SearchedModification < ActiveRecord::Base
   # attr_accessible :title, :body
+  belongs_to :spectrum_identification_protocol
+  validates :spectrum_identification_protocol_id, :presence => true
 end
