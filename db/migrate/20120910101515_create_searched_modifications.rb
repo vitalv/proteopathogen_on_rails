acea2a27f9d139ac0ccc83d9476339cad27cc37c
vitalv <uveuve@gmail.com>
2012-09-10 12:50:31 +0200

    Edited searched_modification model and migration that were created (by mistake) on master. Don't forget to run the migration

--- db/migrate/20120910101515_create_searched_modifications.rb
+++ db/migrate/20120910101515_create_searched_modifications.rb
@@ -1,7 +1,10 @@
 class CreateSearchedModifications < ActiveRecord::Migration
   def change
     create_table :searched_modifications do |t|
-
+      t.references :spectrum_identification_protocol, :null => false
+      t.float :mass_delta
+      t.bool :is_fixed
+      t.string :residue
       t.timestamps
     end
   end
