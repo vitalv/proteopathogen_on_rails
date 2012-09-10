37ba5eb901adecb733eb4e4cc88673082c7b4600
vitalv <uveuve@gmail.com>
2012-09-10 12:50:31 +0200

    Edited searched_modification model and migration that were created (by mistake) on master. Don't forget to run the migration

--- app/models/spectrum_identification_protocol.rb
+++ app/models/spectrum_identification_protocol.rb
@@ -3,4 +3,5 @@ class SpectrumIdentificationProtocol < ActiveRecord::Base
   validates :sip_id, :presence => true
   has_many :spectrum_identification_lists, :dependent => :destroy
   validates_associated :spectrum_identification_lists
+  has_many :searched_modifications, :dependent => :destroy
 end
