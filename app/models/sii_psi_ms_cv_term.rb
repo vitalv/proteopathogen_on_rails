63a1a8886df28549a721346a862bab7614bb2adf
Vital @ UCM <uveuve@gmail.com>
2012-09-11 18:15:50 +0200

    on branch ucm:dbthing. Created models and migrations for sip_psi_ms_cv_terms and sip_user_params. Migrations already run. Affected relationships in models updated accordingly. Algo mas? Ah si, updated model sii_psi_ms_cv_term to validate presence of sii_id

--- app/models/sii_psi_ms_cv_term.rb
+++ app/models/sii_psi_ms_cv_term.rb
@@ -1,4 +1,5 @@
 class SiiPsiMsCvTerm < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :spectrum_identification_item
+  validates :spectrum_identification_item_id, :presence => true
 end
