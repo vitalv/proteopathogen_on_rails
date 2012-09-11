504858d03edfda58207525c7c8488f24751301a6
Vital @ UCM <uveuve@gmail.com>
2012-09-11 16:11:25 +0200

    on branch ucm:dbthing. added some validations in spectrumII and spectrumIR (presence => true things)

--- app/models/spectrum_identification_result.rb
+++ app/models/spectrum_identification_result.rb
@@ -1,6 +1,7 @@
 class SpectrumIdentificationResult < ActiveRecord::Base
   # attr_accessible :title, :body
     validates :sir_id, :presence => true
+    validates :spectrum_identification_list_id,  :presence => true
     belongs_to :spectrum_identification_list
     has_many :spectrum_identification_items, :dependent => destroy
     validates_associated :spectrum_identification_items
