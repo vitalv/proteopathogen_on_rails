ea26a936e1d1d678db601e019246281e14108cb9
Vital @ UCM <uveuve@gmail.com>
2012-09-11 12:37:09 +0200

    on branch ucm:dbthing. Create sii_psi_ms_cv_term model and migration. Migration already run. Updated sii model

--- app/models/spectrum_identification_item.rb
+++ app/models/spectrum_identification_item.rb
@@ -4,4 +4,5 @@ class SpectrumIdentificationItem < ActiveRecord::Base
   belongs_to :spectrum_identification_result
   has_many :peptide_evidences, :dependent => destroy
   validates_associated :peptide_evidences
+  has_many :sii_psi_ms_cv_terms, :dependent => destroy
 end
