277fce3adf076628ea740bb51ac0ad324d605995
Vital @ UCM <uveuve@gmail.com>
2012-09-12 16:28:58 +0200

    corrected some silly thing in the sip_user_param migration. Updated accordingly db/schema.rb

--- db/migrate/20120911160330_create_sip_user_params.rb
+++ db/migrate/20120911160330_create_sip_user_params.rb
@@ -1,10 +1,9 @@
 class CreateSipUserParams < ActiveRecord::Migration
   def change
     create_table :sip_user_params do |t|
-      t.references :spectrum_identification_protocol_id
+      t.references :spectrum_identification_protocol
       t.string :name
       t.string :value
-      t.timestamps
     end
   end
 end
