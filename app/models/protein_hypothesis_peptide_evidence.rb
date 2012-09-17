a8b3db1a741954e8fccd47d6c778a5ecf6179db4
Vital @ UCM <uveuve@gmail.com>
2012-09-17 17:04:51 +0200

    on branch ucm:dbthing. deleted the protein_hypothesis_peptide_evidence join table migration and model. Tengo que rehacerlo con has_and_belongs_to_many entre las partes implicadas. Tambien incluyo aqui de paso la migracion de la join table buena para sii_pepevidence

--- app/models/protein_hypothesis_peptide_evidence.rb
+++
@@ -1,7 +0,0 @@
-class ProteinHypothesisPeptideEvidence < ActiveRecord::Base
-  # attr_accessible :title, :body
-  belongs_to :protein_detection_hypothesis
-  belongs_to :peptide_evidence
-  validates :protein_detection_hypothesis_id, :presence => true
-  validates :peptide_evidence_id, :presence => true
-end
