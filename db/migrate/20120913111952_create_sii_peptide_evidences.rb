7dcffe9659e3a33d582108433cb599dd833926be
Vital @ UCM <uveuve@gmail.com>
2012-09-17 16:44:15 +0200

    on branch ucm:dbthing. deleted the sii_peptide_evidence join table migration and model. Tengo que rehacerlo con has_and_belongs_to_many entre sii y peptide evidence

--- db/migrate/20120913111952_create_sii_peptide_evidences.rb
+++
@@ -1,8 +0,0 @@
-class CreateSiiPeptideEvidences < ActiveRecord::Migration
-  def change
-    create_table :sii_peptide_evidences do |t|
-      t.references :spectrum_identification_item
-      t.references :peptide_evidence
-    end
-  end
-end
