b430a9fa4eaea8c4c895d6aafbc762a4597177c1
Vital @ UCM <uveuve@gmail.com>
2012-09-17 17:24:01 +0200

    on branch ucm:dbthing. Created new join tables (migrations, deleted old models) to use the has_and_belongs_to_many association between sii and pep_evidence; and between pep_evidence and protein_detection_hypothesis

--- app/models/peptide_evidence.rb
+++ app/models/peptide_evidence.rb
@@ -2,6 +2,6 @@ class PeptideEvidence < ActiveRecord::Base
   # attr_accessible :title, :body
   validates :peptide_id, :presence => true
   belongs_to :peptide
-  has_many :sii_peptide_evidences, :dependent => destroy
-  has_many :protein_hypothesis_peptide_evidences ,:dependent => destroy
+  has_and_belongs_to_many :spectrum_identification_items, :join_table => 'sii_pepevidence_join_table'
+  has_and_belongs_to_many :protein_detection_hypothesis, :join_table => 'protein_hypothesis_pepevidence_join_table'
 end
