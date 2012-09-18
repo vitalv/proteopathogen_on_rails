bf256ac4eb809723fb18111996847a57a6e108ce
Vital @ UCM <uveuve@gmail.com>
2012-09-18 11:24:32 +0200

    on branch ucm:dbthing. Model and migration sir_user_params. Migrations run.

--- db/migrate/20120917151851_protein_hypothesis_pepevidence_join_table.rb
+++
@@ -1,8 +0,0 @@
-class ProteinHypothesisPepevidenceJoinTable < ActiveRecord::Migration
-  def change
-    create_table :protein_hypothesis_pepevidence_join_table, :id => false do |t|
-      t.integer :protein_detection_hypothesis_id
-      t.integer :peptide_evidence_id
-    end
-  end
-end
