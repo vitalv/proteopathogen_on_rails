07c4ed01fbcb5cd72eb4b44b8a6f6c7fca1f5429
Vital @ UCM <uveuve@gmail.com>
2012-10-15 15:55:58 +0200

    sip things

--- lib/mzid_2_db.rb
+++ lib/mzid_2_db.rb
@@ -11,16 +11,30 @@ class Mzid2db
     #~ puts @mzid_obj.sips[0].input_spectra
     #~ puts @mzid_obj.sips[0].sip_id
     @mzid_obj.sips.each do |sip|
-      puts "SIP: #{sip.sip_id} - Software: #{sip.analysis_software} - Search type: #{sip.search_type}"
-      puts "\tSearched Modifications:"
+    
+      puts "\nSIP: #{sip.sip_id} - Software: #{sip.analysis_software} - Search type: #{sip.search_type}"
+      sip_id = sip.sip_id
+      activity_date = "DE:DONDE:SACO:ESTO"
+      input_spectra = sip.input_spectra
+      search_db = 
+      this_SIP = SpectrumIdentificationProtocol.new(:sip_id, :activity_date, :input_spectra
+      
+      puts "\n\tSearched Modifications:"
       sip.searched_modification_arr.each do |struct|
         puts "\tMassD: #{struct.mass_delta} - Fixed: #{struct.fixedMod} - Residue: #{struct.residue} - UnimodAc: #{struct.unimod_accession}"
       end
-      puts "\tPSI-MS terms:"
+      
+      puts "\n\tSearch DB:"
+      sip.search_db_arr.each do |sdb_struct| #(:name, :location, :version, :releaseDate, :num_seq)
+        puts "\tName: #{sdb_struct.name} - Location: #{sdb_struct.location} - Version : #{sdb_struct.version}"
+      end     
+      
+      puts "\n\tPSI-MS terms:"
       sip.psi_ms_terms.each do |h|
         puts "\tAccession: #{h[:accession]} - Name: #{h[:name]} - Value: #{h[:value]}"
       end      
-      puts "\tUser Params:"
+      
+      puts "\n\tUser Params:"
       sip.user_params.each do |h|
         puts "\tName: #{h[:name]} - Value: #{h[:value]}"
       end        
