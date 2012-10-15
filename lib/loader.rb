a9b512dc3d40da47b87957785157d5f57bf3d7c3
Vital @ UCM <uveuve@gmail.com>
2012-10-15 18:49:41 +0200

    added require rails environment so I can run the thing standalone using the rails models

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/ruby
 
 #ruby loader.rb /home/vital/pepXML_protXML_2_mzid_V/SILAC_phos_OrbitrapVelos_1_interact-ipro-filtered.mzid
+require "#{File.dirname(__FILE__)}/../config/environment.rb"
+#require './config/environment.rb'
 
-require 'rubygems'
+require 'bundler/setup'
+$: << 'lib/'
 require 'mzid_parser'
 require 'mzid_2_db'
 
 begin 
 
   mzid_object = Mzid.new(ARGV[0])
+  #mzid_object = Mzid.new("/home/vital/pepXML_protXML_2_mzid_V/SILAC_phos_OrbitrapVelos_1_interact-ipro-filtered.mzid")
   
   Mzid2db.new(mzid_object).save2tables
   
