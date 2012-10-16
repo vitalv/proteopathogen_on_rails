403e2c6d3aa57688714e2ca3952b8bbbb886194f
Vital @ UCM <uveuve@gmail.com>
2012-10-16 12:39:14 +0200

    tryin to save to searchDatabase

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/ruby
 
+#rvm use 1.9.3@rails3gemset
 #ruby loader.rb /home/vital/pepXML_protXML_2_mzid_V/SILAC_phos_OrbitrapVelos_1_interact-ipro-filtered.mzid
+
 require "#{File.dirname(__FILE__)}/../config/environment.rb"
 #require './config/environment.rb'
 
