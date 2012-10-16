7385c95929db39f8b505edc374cc4b3ad08748b6
Vital @ UCM <uveuve@gmail.com>
2012-10-16 15:04:46 +0200

    add validates location presence true in search_database table

--- app/models/search_database.rb
+++ app/models/search_database.rb
@@ -1,4 +1,5 @@
 class SearchDatabase < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :spectrum_identification_protocols
+  validates :location, :presence => true #attribute location is required in <SearchDatabase> in the mzid file
 end
