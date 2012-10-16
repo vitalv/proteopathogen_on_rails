e46d9c0a1413f426b2e30957c8ea589e79b78b33
Vital @ UCM <uveuve@gmail.com>
2012-10-16 18:56:06 +0200

    I can now insert to spectrum_identification_protocols and to search_databases. had to remove other associations from SIP model to make it work

--- app/models/spectrum_identification_protocol.rb
+++ app/models/spectrum_identification_protocol.rb
@@ -2,8 +2,8 @@ class SpectrumIdentificationProtocol < ActiveRecord::Base
   # attr_accessible :title, :body
   validates :sip_id, :presence => true
   belongs_to :search_database
-  has_many :spectrum_identification_lists, :dependent => :destroy
-  validates_associated :spectrum_identification_lists
+  #has_many :spectrum_identification_lists, :dependent => :destroy
+  #validates_associated :spectrum_identification_lists
   has_many :searched_modifications, :dependent => :destroy
   has_many :sip_psi_ms_cv_terms, :dependent => :destroy
   has_many :sip_user_params, :dependent => :destroy
--- lib/loader.rb
+++ lib/loader.rb
@@ -3,11 +3,11 @@
 #rvm use 1.9.3@rails3gemset
 #ruby loader.rb /home/vital/pepXML_protXML_2_mzid_V/SILAC_phos_OrbitrapVelos_1_interact-ipro-filtered.mzid
 
-require "#{File.dirname(__FILE__)}/../config/environment.rb"
-#require './config/environment.rb'
+require File.expand_path(File.dirname(__FILE__)) + "/../config/environment.rb"
+#require '../config/environment.rb'
 
 require 'bundler/setup'
-$: << 'lib/'
+#$: << 'lib/'
 require 'mzid_parser'
 require 'mzid_2_db'
 
@@ -18,10 +18,10 @@ begin
   
   Mzid2db.new(mzid_object).save2tables
   
-  rescue Exception => msg
-  puts msg
-  puts msg.backtrace
-  puts msg.inspect
+  #~ rescue Exception => msg
+  #~ puts msg
+  #~ puts msg.backtrace
+  #~ puts msg.inspect
   
 
 end
