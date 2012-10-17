23f05413b1acbd14406c70dfe41a1efa22ed5a07
Vital @ UCM <uveuve@gmail.com>
2012-10-17 17:00:43 +0200

    fixed the habtm sip_sdb_join_table insertion thing

--- lib/mzid_2_db.rb
+++ lib/mzid_2_db.rb
@@ -5,7 +5,6 @@ class Mzid2db
   end
 
   def save2tables
-    
 
     @mzid_obj.sips.each do |sip|
     
@@ -20,19 +19,15 @@ class Mzid2db
       psi_ms_terms = sip.psi_ms_terms
       user_params = sip.user_params
       
-      search_db_arr.each do |sdb| 
-        this_sdb = SearchDatabase.find_or_create_by_name_and_version_and_release_date_and_number_of_sequences_and_location
-        (:name => sdb.name, :version => sdb.version, :release_date => sdb.releaseDate, :number_of_sequences => sdb.num_seq, :location => sdb.location)
-        #Save one SIP per each (usually one) of its SDBs. Different SDBs mean completely new SIP (NEW SEARCHES) !!
-        #SpectrumIdentificationProtocol.new(:sip_id => sip_id, :input_spectra => input_spectra,
-        #:analysis_software => analysis_software, :search_type => search_type, :threshold => threshold,
-        # :search_database_id => this_sdb.id).save
-        SpectrumIdentificationProtocol.find_or_create_by_sip_id_and_input_spectra_and_analysis_software_and_search_type_and_threshold_and_search_database_id)
-        (:sip_id => sip_id, :input_spectra => input_spectra,:analysis_software => analysis_software, :search_type => search_type, :threshold => threshold, :search_database_id => this_sdb.id)
-        
-      end
-  
+      this_sip = SpectrumIdentificationProtocol.find_or_create_by_sip_id_and_input_spectra_and_analysis_software_and_search_type_and_threshold(:sip_id => sip_id, :input_spectra => input_spectra, :analysis_software => analysis_software, :search_type => search_type, :threshold => threshold)
       
+      search_db_arr.each do |sdb|
+        this_sdb = SearchDatabase.find_or_create_by_name_and_version_and_release_date_and_number_of_sequences_and_location(:name => sdb.name, :version => sdb.version, :release_date => sdb.releaseDate, :number_of_sequences => sdb.num_seq, :location => sdb.location)
+        #HABTM. sip_sdb_join_table
+        this_sip.search_databases << this_sdb #unless this_sip.search_databases.include? this_sdb
+      end
+      
+   
     end
   end
 
