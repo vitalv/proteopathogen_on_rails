bd3a28a9802e4408003c107e70b64c4f9fa263bd
Vital Vialas <uveuve@gmail.com>
2012-10-21 20:24:37 +0200

    created spectra_acquisition_Run controller and view

---
+++ app/controllers/spectra_acquisition_runs_controller.rb
@@ -0,0 +1,13 @@
+class SpectraAcquisitionRunsController < ApplicationController
+
+def index
+
+  puts "Guardar en una variable toda la lista de SARs para ser visualizada enel view" 
+
+end
+
+def new
+  #Aqui es donde pondre el formulario, un boton para subir el mzid y correr mi codigo save2tables
+end
+
+end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,3 +1,4 @@
 class Sample < ActiveRecord::Base
   # attr_accessible :title, :body
+  :has_many :spectra_acquisition_runs
 end
