21b7c6a467b1b28a6314e2f9e0862efd34b14ae3
Vital @ UCM <uveuve@gmail.com>
2012-10-25 16:42:51 +0200

    added the rescue and rollback part

--- app/controllers/mzid_files_controller.rb
+++ app/controllers/mzid_files_controller.rb
@@ -42,35 +42,10 @@ class MzidFilesController < ApplicationController
     
     mzid_object = Mzid.new(location)
  
-
-
-
-   #--------------------------------
-      #~ sip = mzid_object.sips[0]
-      #~ sip_id = sip.sip_id
-      #~ input_spectra = sip.input_spectra      
-      #~ analysis_software = sip.analysis_software
-      #~ search_type = sip.search_type
-      #~ threshold = sip.threshold      
-      #~ search_db_arr = sip.search_db_arr
-      #~ searched_mod_arr = sip.searched_modification_arr
-      #~ psi_ms_terms = sip.psi_ms_terms
-      #~ parent_tol_plus_value, parent_tol_minus_value = sip.parent_tolerance[0][:value], sip.parent_tolerance[1][:value]
-      #~ fragment_tol_plus_value, fragment_tol_minus_value = sip.fragment_tolerance[0][:value], sip.fragment_tolerance[1][:value]
-      #~ user_params = sip.user_params
-      #~ spectra_acquisition_run_id = SpectraAcquisitionRun.find_by_spectra_file(input_spectra).id 
-      #~ @this_sip = SpectrumIdentificationProtocol.create(:sip_id => sip_id, :input_spectra => input_spectra, :spectra_acquisition_run_id => spectra_acquisition_run_id, :analysis_software => analysis_software, :search_type => search_type, :threshold => threshold, :parent_tol_plus_value => parent_tol_plus_value, :parent_tol_minus_value => parent_tol_minus_value, :fragment_tol_plus_value => fragment_tol_plus_value, :fragment_tol_minus_value => fragment_tol_minus_value, :mzid_file_id => saved_mzid_id)   
-   #--------------------------------
-
-
-
-
-
- 
     Mzid2db.new(mzid_object, saved_mzid_id).save2tables
-#    rescue Exception => msg
-#      puts "\n#{msg}"
-#      rollback(@sample_id)
+    rescue Exception => msg
+      puts "\n#{msg}"
+      rollback(@sample_id)
 
   end
 
