88d5e06927006aad817b486b1a7d29a523e58ca6
Vital @ UCM <uveuve@gmail.com>
2012-11-06 13:02:38 +0100

    added the experiment views that were under samples before

--- app/models/mzid_file.rb
+++ app/models/mzid_file.rb
@@ -3,6 +3,7 @@ class MzidFile < ActiveRecord::Base
   validates :location, :presence => true
   validates :sha1, :uniqueness => true
   validates :name, :presence => true
+  belongs_to :experiment
   has_many :spectra_acquisition_runs, :dependent => :destroy
   validates_associated :spectra_acquisition_runs
 end
