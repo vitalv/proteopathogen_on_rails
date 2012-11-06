505287ef4ee08a301446eede5043f37096b70745
Vital @ UCM <uveuve@gmail.com>
2012-11-06 13:11:09 +0100

    deleted some samples left-overs

--- app/models/sample.rb
+++
@@ -1,6 +0,0 @@
-class Sample < ActiveRecord::Base
-  # attr_accessible :title, :body
-  has_many :spectra_acquisition_runs
-  validates :organism, :presence => true
-  validates :protocol, :presence => true
-end
