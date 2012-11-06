95c1f420c564f0a0b86d7b6dfffa9f3ae1e70ed5
Vital @ UCM <uveuve@gmail.com>
2012-11-06 13:05:52 +0100

    added the content of the former samples controller to experiments_controller

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,2 +1,34 @@
 class ExperimentsController < ApplicationController
+
+  def index
+ 
+    @all_experiments = Experiment.find(:all)
+ 
+  end
+
+
+  def new
+
+    @experiment = Experiment.new
+  
+  end
+
+
+  def create
+
+    @saved_experiment = Experiment.create(params[:experiment])
+    if @saved_experiment.invalid?
+      @saving_experiment_errors = @saved_experiment.errors
+      @experiment = @saved_experiment
+      render :action => "new"
+    else
+      saved_experiment_id = @saved_experiment.id
+      redirect_to experiment_spectra_acquisition_runs_path(saved_experiment_id) #this goes to experiment/:id/spectra_acquisition_runs/index
+    end
+
+  end
+
+
+
+
 end
