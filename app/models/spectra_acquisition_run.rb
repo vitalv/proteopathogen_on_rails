244976a480a77322f6a6b53fe8ca2c789bf15363
Vital @ UCM <uveuve@gmail.com>
2012-11-06 12:51:47 +0100

    deleted 'sample' files and added 'experiment' files

--- app/models/spectra_acquisition_run.rb
+++ app/models/spectra_acquisition_run.rb
@@ -2,6 +2,5 @@ class SpectraAcquisitionRun < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :spectrum_identification_protocols, :dependent => :destroy
   validates :spectra_file, :uniqueness => true
-  belongs_to :sample
   belongs_to :mzid_file
 end
