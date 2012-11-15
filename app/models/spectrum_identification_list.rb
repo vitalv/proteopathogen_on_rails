78a2cd51ef3f5db5754249b0810aa23efe6f2c6c
Vital @ UCM <uveuve@gmail.com>
2012-11-15 17:01:30 +0100

    spectrum_identifications method in mzid_parser.rb gives array of si objects y bla bla

--- app/models/spectrum_identification_list.rb
+++ app/models/spectrum_identification_list.rb
@@ -1,6 +1,6 @@
 class SpectrumIdentificationList < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :spectrum_identification
-  has_many :spectrum_identification_results, :dependent => destroy
+  has_many :spectrum_identification_results, :dependent => :destroy
   validates_associated :spectrum_identification_results
 end
