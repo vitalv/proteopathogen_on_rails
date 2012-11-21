7fd45370a02378e0853041cbd3c3a1e1402f4e21
Vital @ UCM <uveuve@gmail.com>
2012-11-21 16:24:17 +0100

    save2tables in mzid_2_db.rb now saves ok to si, sip and sil

--- app/models/spectrum_identification.rb
+++ app/models/spectrum_identification.rb
@@ -3,13 +3,12 @@ class SpectrumIdentification < ActiveRecord::Base
   has_and_belongs_to_many :spectra_acquisition_runs, :join_table => :sar_si_join_table
   has_and_belongs_to_many :search_databases, :join_table => :sdb_si_join_table
   
-  has_one :spectrum_identification_protocol
-  validates :spectrum_identification_protocol_id, :presence => true
+  has_one :spectrum_identification_protocol, :dependent => :destroy
   validates_associated :spectrum_identification_protocol
   
-  has_one :spectrum_identification_list
-  validates :spectrum_identification_list_id, :presence => true
+  has_one :spectrum_identification_list, :dependent => :destroy
   validates_associated :spectrum_identification_list
+  
   has_many :spectrum_identification_results, :through => :spectrum_identification_list, :dependent => :destroy
   
   
