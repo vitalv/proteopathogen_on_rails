2dcc2f86f20f199844ddd6f7594bd8802fdd9ee0
Vital @ UCM <uveuve@gmail.com>
2013-02-27 18:04:24 +0100

    the splitSave2Tables thing seems to work ok. some testing may be still needed

--- lib/mzid_2_db.rb
+++ lib/mzid_2_db.rb
@@ -1,19 +1,18 @@
 class Mzid2db
 
 #mzid = Mzid.new("/home/vital/proteopathogen_on_rails_3/proteopathogen_on_rails/public/uploaded_mzid_files/SILAC_phos_OrbitrapVelos_1_interact-ipro-filtered.mzid"); nil;
-#mzid = Mzid.new("/home/vital/proteopathogen_on_rails_3/proteopathogen_on_rails/public/uploaded_mzid_files/examplefile.mzid"), nil;
+#mzid = Mzid.new("/home/vital/proteopathogen_on_rails_3/proteopathogen_on_rails/public/uploaded_mzid_files/examplefile.mzid"); nil;
 
   def initialize(mzid_object)
     @mzid_obj = mzid_object
     @mzid_file_id = mzid_object.mzid_file_id
   end
-
-
-  ##ATENÇAO!! Sería interesante hacer una funcion experiment_rollback que borre el ultimo experimento que he metido con todos los datos de todas las tablas (no se borrara nada en las de SIP-scope que tengan otros SIP asociados)!!
-  #La idea es que una vez que se empieza a insertar datos (un experimento/busqueda) se llenen TOODAS las tablas y que no se quede a medias. Y si no se completa el proceso hacer un rollback automatico
-  #ENtonces tendría que controlar que no se vuelve a insertar el mismo experimento para
-  #usar  find_or_create SOLO en aquellas tablas con Global-scope  (que pueden tener records iguales en distintos SIP:experimentos/busquedas) como search_databases o peptides, etc..
-
+  
+  #Note on Variable naming :
+  #This script deals with the objects from mzid_parser (@mzid_obj) 
+  #used to create new objects (to be saved to the db) that refer to the same concept
+  #(i.e. Create Sip object from @mzid_obj.sip object)
+  #objects from mzid_parser are prefixed mzid_  ;  objects created here to be saved are prefixed my_
 
   def save2tables
   
@@ -48,8 +47,9 @@ class Mzid2db
             my_PeptideEvidence = savePeptideEvidence(pep_ev_ref, pep_id, dbseq_id)
             saveSiiPepEvidences(my_item, my_PeptideEvidence)            
           end          
-          saveFragments(mzid_item, my_item) #puedo hacer el bucle en la funcion
-          
+          saveFragments(mzid_item, my_item) 
+          saveSiiPsiMsCvTerms(mzid_item, my_item)
+          saveSiiPsiMsCvTerms(mzid_item, my_item)
         end
       end
     end
@@ -86,13 +86,14 @@ class Mzid2db
   
   
   def saveSip(mzid_si, my_si)
+    sip = @mzid_obj.sip(mzid_si.sip_ref)
     sip_id = sip.sip_id
     analysis_software = sip.analysis_software
     search_type = sip.search_type
     threshold = sip.threshold
     parent_tol_plus_value, parent_tol_minus_value = sip.parent_tolerance[0][:value], sip.parent_tolerance[1][:value]
     fragment_tol_plus_value, fragment_tol_minus_value = sip.fragment_tolerance[0][:value], sip.fragment_tolerance[1][:value]
-    my_sip = SpectrumIdentificationProtocol.create(:spectrum_identification_id => si_id, :sip_id => sip_id, :analysis_software => analysis_software, :search_type => search_type, :threshold => threshold, :parent_tol_plus_value => parent_tol_plus_value, :parent_tol_minus_value => parent_tol_minus_value, :fragment_tol_plus_value => fragment_tol_plus_value, :fragment_tol_minus_value => fragment_tol_minus_value)
+    my_sip = SpectrumIdentificationProtocol.create(:spectrum_identification_id => my_si.id, :sip_id => sip_id, :analysis_software => analysis_software, :search_type => search_type, :threshold => threshold, :parent_tol_plus_value => parent_tol_plus_value, :parent_tol_minus_value => parent_tol_minus_value, :fragment_tol_plus_value => fragment_tol_plus_value, :fragment_tol_minus_value => fragment_tol_minus_value)
     return my_sip
   end
  
@@ -194,15 +195,13 @@ class Mzid2db
     calc_m2z, exp_m2z = mzid_item.calc_m2z, mzid_item.exp_m2z
     rank, charge_state  = mzid_item.rank, mzid_item.charge_state
     pass_threshold = mzid_item.pass_threshold    
-    #pepEv_ref_arr = item.pepEv_ref_arr    
-    #sii_psi_ms_cv_terms, sii_user_params = item.sii_psi_ms_cv_terms, item.sii_user_params    
     my_item = SpectrumIdentificationItem.create(:sii_id => sii_id, :spectrum_identification_result_id => spectrum_identification_result_id, :calc_m2z => calc_m2z, :exp_m2z => exp_m2z, :rank => rank, :charge_state => charge_state, :pass_threshold => pass_threshold )   
     return my_item
   end
 
 
   def savePeptide(pep_ev_ref)
-    pep_ev = @mzid_obj.pep_evidence(pepEv_ref)
+    pep_ev = @mzid_obj.pep_evidence(pep_ev_ref)
     pep_ref = pep_ev.pep_ref
     pep = @mzid_obj.pep(pep_ref)
     pep_seq = pep.sequence  
@@ -212,7 +211,7 @@ class Mzid2db
 
 
   def saveDbSequence(pep_ev_ref, sil_id)
-    dbseq_ref = pep_ev.db_seq_ref
+    dbseq_ref = @mzid_obj.pep_evidence(pep_ev_ref).db_seq_ref
     db_seq = @mzid_obj.db_seq(dbseq_ref)
     db_seq_accession, db_seq_description = db_seq.accession, db_seq.description
     db_seq_sequence = db_seq.sequence
@@ -232,12 +231,12 @@ class Mzid2db
 
 
   def savePeptideEvidence(pep_ev_ref, pep_id, dbseq_id)  
+    #Esto no guardarlo siempre. P.ej:
+    #peptide_1_1_SDB_1_orf19.1086_554_569 se refiere al sii: "SIL_AtiO2_SII_1_1" y también al sii: "SIL_Elu1A_SII_2_1"
     pep_ev = @mzid_obj.pep_evidence(pep_ev_ref)
-    my_PeptideEvidence = PeptideEvidence.find_or_create_by_peptide_id_and_db_sequence_id_and_start_and_end(:peptide_id => pep_id, :db_sequence_id => db_seq_id, :start => pep_ev.start, :end => pep_ev.end)
-    if my_PeptideEvidence.new_record?
-       my_PeptideEvidence.pre, this_PeptideEvidence.post = pep_ev.pre, pep_ev.post
-       my_PeptideEvidence.is_decoy = pep_ev.is_decoy
-    end
+    my_PeptideEvidence = PeptideEvidence.find_or_initialize_by_peptide_id_and_db_sequence_id_and_start_and_end_and_is_decoy(:peptide_id => pep_id, :db_sequence_id => dbseq_id, :start => pep_ev.start, :end => pep_ev.end, :is_decoy => pep_ev.is_decoy)
+    my_PeptideEvidence.pre, my_PeptideEvidence.post = pep_ev.pre, pep_ev.post if my_PeptideEvidence.new_record?
+    my_PeptideEvidence.save if my_PeptideEvidence.new_record?
     return my_PeptideEvidence
   end
   
@@ -257,131 +256,36 @@ class Mzid2db
     end
   end
 
-    ##SAVE SILs, SIRs, SIIs and STUFF##---------------------------------- #Elements <AnalysisData><SpectrumIdentificationList>
-    #spectrum_identification_items_ids = []
-    spectrum_identification_lists_ids.each do |sil_id|
-      sil_ref = SpectrumIdentificationList.find(sil_id).sil_id
-      @results_arr = @mzid_obj.spectrum_identification_results(sil_ref)
-      @results_arr.each do |sir|
-        #~ sir_id = sir.sir_id
-        #~ spectrum_identification_list_id = SpectrumIdentificationList.find_by_sil_id(sil_ref).id
-        #~ spectrum_id = sir.spectrum_id
-        #~ spectrum_name = sir.spectrum_name
-        #~ sir_psi_ms_cv_terms = sir.sir_psi_ms_cv_terms
-        #~ sir_user_params = sir.sir_user_params
-        #~ this_sir = SpectrumIdentificationResult.create(:sir_id => sir_id, :spectrum_identification_list_id => spectrum_identification_list_id, :spectrum_id => spectrum_id, :spectrum_name => spectrum_name)
-        #~ unless sir_psi_ms_cv_terms.empty?
-          #~ sir_psi_ms_cv_terms.each do |psi_ms_t|
-            #~ #this_psi_term = SirPsiMsCvTerm.find_or_initialize_by_spectrum_identification_result_id_and_psi_ms_cv_term_accession(:spectrum_identification_result_id => this_sir.id, :psi_ms_cv_term_accession => psi_ms_t[:accession])
-            #~ #this_psi_term.value = psi_ms_t[:value] if this_psi_term.new_record? unless psi_ms_t[:value].blank?
-            #~ 
-            #~ 
-            #~ ##WATCH OUT: SAVE ONLY IF NEW (that is, NEW in GLOBAL scope, the whole table) RECORD 
-            #~ 
-            #~ SirPsiMsCvTerm.create(:spectrum_identification_result_id => this_sir.id, :psi_ms_cv_term => psi_ms_t[:accession], :value => psi_ms_t[:value])
-            #~ #this_psi_term.save           
-          #~ end
-        #~ end
-        #~ unless sir_user_params.empty?
-          #~ sir_user_params.each do |userP|
-            #~ #this_userP = SirUserParam.find_or_initialize_by_spectrum_identification_result_id_and_name(:spectrum_identification_result_id => this_sir.id, :name => userP[:name])
-            #~ #this_userP.value = userP[:value] if this_userP.new_record? unless userP[:value].blank?
-            #~ SirUserParam.create(:spectrum_identification_result_id => this_sir.id, :name => userP[:name], :value => userP[:value])
-            #~ #this_userP.save
-          #~ end
-        #~ end
- #~ 
-        #~ sir.items_arr.each do |item|
-          #~ sii_id = item.sii_id
-          #~ spectrum_identification_result_id = SpectrumIdentificationResult.find_by_sir_id(this_sir.sir_id).id
-          #~ calc_m2z, exp_m2z = item.calc_m2z, item.exp_m2z
-          #~ rank, charge_state  = item.rank, item.charge_state
-          #~ pass_threshold = item.pass_threshold
-          #~ pepEv_ref_arr = item.pepEv_ref_arr
-          #~ sii_psi_ms_cv_terms, sii_user_params = item.sii_psi_ms_cv_terms, item.sii_user_params
- #~ 
-          #~ #--- save SpectrumIdentificationItem ---
-          #~ this_item = SpectrumIdentificationItem.create(:sii_id => sii_id, :spectrum_identification_result_id => spectrum_identification_result_id, :calc_m2z => calc_m2z, :exp_m2z => exp_m2z, :rank => rank, :charge_state => charge_state, :pass_threshold => pass_threshold ) 
-          #~ #Save Peptide_Evidences, Peptides And DbSequences ---------
-          #~ #OptionA: with my pep_evidence I get a dBSEq_ref and a peptide_ref
-          #~ pepEv_ref_arr.each do |pepEv_ref|
-            #~ #get mzid.pep_evidence:  PepEv = #<struct PepEv pepEv_id=nil, start=nil, end=nil, pre=nil, post=nil, is_decoy=nil, db_seq_ref=nil, name=nil, pep_ref=nil>
-            #~ pep_ev = @mzid_obj.pep_evidence(pepEv_ref)
-            #~ pep_ref = pep_ev.pep_ref
-            #~ #get mzid.pep: Object Pep ( 
-            #~ pep = @mzid_obj.pep(pep_ref)
-            #~ pep_seq = pep.sequence
-      #~ 
-            #~ #--- save Peptide ---
-            #~ this_Peptide = Peptide.find_or_create_by_sequence(:sequence => pep_seq, :peptide_id => pep_ref)
-            #~ 
-            #~ dbseq_ref = pep_ev.db_seq_ref
-            #~ db_seq = @mzid_obj.db_seq(dbseq_ref)
-            #~ db_seq_accession, db_seq_description = db_seq.accession, db_seq.description
-            #~ db_seq_sequence = db_seq.sequence
-            #~ search_db_ref = db_seq.search_db_ref
-            #~ 
-            #~ #get search_db_id(s) (through sil_id):
-            #~ si_id = SpectrumIdentificationList.find(sil_id).spectrum_identification_id
-            #~ search_db_id = nil #validate presence in model 
-            #~ SpectrumIdentification.find(si_id).search_databases.each { |sdb| search_db_id = sdb.id if sdb.sdb_id == search_db_ref }
-#~ 
-            #~ #--- save DbSequence ---
-            #~ this_DbSequence = DbSequence.find_or_initialize_by_accession_and_sequence(:accession => db_seq_accession, :sequence => db_seq_sequence)
-            #~ if this_DbSequence.new_record?
-              #~ this_DbSequence.description  = db_seq_description
-              #~ this_DbSequence.search_database_id = search_db_id 
-              #~ this_DbSequence.save
-            #~ end
-            #~ #--- save Peptide_Evidence
-            #~ this_PeptideEvidence = PeptideEvidence.find_or_create_by_peptide_id_and_db_sequence_id_and_start_and_end(:peptide_id => this_Peptide.id, :db_sequence_id => this_DbSequence.id, :start => pep_ev.start, :end => pep_ev.end)
-            #~ if this_PeptideEvidence.new_record?
-              #~ this_PeptideEvidence.pre, this_PeptideEvidence.post = pep_ev.pre, pep_ev.post
-              #~ this_PeptideEvidence.is_decoy = pep_ev.is_decoy
-            #~ end
-            #~ this_item.peptide_evidences << this_PeptideEvidence unless this_item.peptide_evidences.include? this_PeptideEvidence
-            #~ 
-          #~ end #pepEv_ref_arr.each do |pepEv_ref|
-          
-          #--- save Fragments -----------------------------------------
-          unless item.fragments_arr.empty?
-            item.fragments_arr.each do |f|
-              Fragment.create(:spectrum_identification_item_id => this_item.id, :charge => f.charge, :index => f.ion_index, :m_mz => f.mz_value, :m_intensity => f.m_intensity, :m_error => f.m_err, :fragment_type => f.fragment_name, :psi_ms_cv_fragment_type_accession => f.fragment_psi_ms_cv_acc)
-            end
-          end
-         
-          #otra vez esto: (??) No puedes secarlo un poco?? (Sí, "secarlo", ya sabes, ;-) , ;-)  )
-          unless sii_psi_ms_cv_terms.empty?
-            sii_psi_ms_cv_terms.each do |psi_ms_t|
-              #this_psi_term = SiiPsiMsCvTerm.find_or_initialize_by_spectrum_identification_item_id_and_psi_ms_cv_term_accession(:spectrum_identification_item_id => this_item.id, :psi_ms_cv_term_accession => psi_ms_t[:accession])
-              #this_psi_term.value = psi_ms_t[:value] if this_psi_term.new_record? unless psi_ms_t[:value].blank?
-              #this_psi_term.save
-              #This is more local-scope
-              SiiPsiMsCvTerm.create(:spectrum_identification_item_id => this_item.id, :psi_ms_cv_term_accession => psi_ms_t[:accession], :value => psi_ms_t[:value])
-            end
-          end
-          unless sii_user_params.empty?
-            sii_user_params.each do |userP|
-              #this_userP = SiiUserParam.find_or_initialize_by_spectrum_identification_item_id_and_name(:spectrum_identification_item_id => this_item.id, :name => userP[:name])
-              #this_userP.value = userP[:value] if this_userP.new_record? unless userP[:value].blank?
-              #this_userP.save
-              SiiUserParam.create(:spectrum_identification_item_id => this_item.id, :name => userP[:name], :value => userP[:value])
-            end
-          end
-          
-
-
-          
-        end #items_arr.each do |item|
-        
-      end #results.each do |sir|
     
-    end #spectrum_identification_lists_ids.each do |sil_id|
-   
+  def saveSiiPsiMsCvTerms(mzid_item, my_item)
+    #Again?? Wouldn't you DRY it a little (Yeah, "DRY", you know, ;-) , ;-)  )
+    #sii_psi_ms_cv_terms, sii_user_params = item.sii_psi_ms_cv_terms, item.sii_user_params    
+    sii_psi_ms_cv_terms = mzid_item.sii_psi_ms_cv_terms
+    unless sii_psi_ms_cv_terms.empty?
+      sii_psi_ms_cv_terms.each do |psi_ms_t|
+        #this_psi_term = SiiPsiMsCvTerm.find_or_initialize_by_spectrum_identification_item_id_and_psi_ms_cv_term_accession(:spectrum_identification_item_id => this_item.id, :psi_ms_cv_term_accession => psi_ms_t[:accession])
+        #this_psi_term.value = psi_ms_t[:value] if this_psi_term.new_record? unless psi_ms_t[:value].blank?
+        #this_psi_term.save
+        #This is more local-scope
+        SiiPsiMsCvTerm.create(:spectrum_identification_item_id => my_item.id, :psi_ms_cv_term_accession => psi_ms_t[:accession], :value => psi_ms_t[:value])
+      end
+    end
+  end 
+ 
 
-  end # def save2tables
-
-
+  def saveSiiUserParams(mzid_item, my_item)
+    unless sii_user_params.empty?
+      sii_user_params.each do |userP|
+        #this_userP = SiiUserParam.find_or_initialize_by_spectrum_identification_item_id_and_name(:spectrum_identification_item_id => this_item.id, :name => userP[:name])
+        #this_userP.value = userP[:value] if this_userP.new_record? unless userP[:value].blank?
+        #this_userP.save
+        SiiUserParam.create(:spectrum_identification_item_id => this_item.id, :name => userP[:name], :value => userP[:value])
+      end
+    end  
+  end
+    
+    
+  
 
 
 end
@@ -406,17 +310,18 @@ end
       #puts "\n-Error saving data 2 tables. Rolling back -- \n\n"
       MzidFile.find(mzid_file_id).spectra_acquisition_runs.each do |sar|
         sar.spectrum_identifications.each do |si|
-          sip_id = si.spectrum_identification_protocol.id
-          SpectrumIdentificationProtocol.destroy(sip_id)
+          sip_id = si.spectrum_identification_protocol.id if si.spectrum_identification_protocol
+          SpectrumIdentificationProtocol.destroy(sip_id) if sip_id #SipPsiMsCvTerms and SipUserParams are :dependent => :destroy on sip.destroy
           si.spectrum_identification_results.each do |sir|
             sir.spectrum_identification_items.each do |sii|
-              sii.peptide_evidences.each do |pep_ev|
-                PeptideEvidence.destroy(pep_ev.id)
+              sii.peptide_evidences.each do |pep_ev| #habtm association btwn sii and pep_ev does not support :dependent => :destroy so do it here:
+                PeptideEvidence.destroy(pep_ev.id) #peptide is dependent so destroyed here as well
               end
+              #sii.fragments #fragments are :dependent => :destroy on sii so don't have to destroy 
             end
             SpectrumIdentificationResult.destroy(sir.id)
           end
-          SpectrumIdentification.destroy(si.id)
+          SpectrumIdentification.destroy(si.id) if SpectrumIdentification.exists? (si.id)
         end
         #SpectraAcquisitionRun.destroy(sar.id)
       end
@@ -427,12 +332,3 @@ end
   end
 
 
-#~ class String
-  #~ def camelcase2underscore
-    #~ self.gsub(/::/, '/').
-    #~ gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-    #~ gsub(/([a-z\d])([A-Z])/,'\1_\2').
-    #~ tr("-", "_").
-    #~ downcase
-  #~ end
-#~ end
