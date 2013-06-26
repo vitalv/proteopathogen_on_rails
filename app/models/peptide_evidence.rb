e649d9650575a1f114f45f318d98a56a15fae941
Vital @ UCM <uveuve@gmail.com>
2013-06-26 13:55:47 +0200

    added dependent destroy in the peptide_evidence belongs to peptide association

--- app/models/peptide_evidence.rb
+++ app/models/peptide_evidence.rb
@@ -3,7 +3,7 @@ class PeptideEvidence < ActiveRecord::Base
   validates :peptide_id, :presence => true 
   validates_uniqueness_of :peptide_id, :scope => [:db_sequence_id, :start, :end]
   #There MUST only be one PeptideEvidence item per Peptide-to-DBSequence-position
-  belongs_to :peptide
+  belongs_to :peptide, :dependent => :destroy
   belongs_to :db_squence
   has_and_belongs_to_many :spectrum_identification_items, :join_table => 'sii_pepevidence_join_table'
   #has_and_belongs_to_many :protein_detection_hypothesis, :join_table => 'protein_hypothesis_pepevidence_join_table'
