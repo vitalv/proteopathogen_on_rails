ed738ddb5e381aaf50ef0906654bc3aec55ab87d
Vital @ UCM <uveuve@gmail.com>
2013-06-28 13:43:44 +0200

    test modifications , also deleted som fixtures and tests related to non-existing tables

--- app/models/protein_detection_hypothesis.rb
+++ app/models/protein_detection_hypothesis.rb
@@ -4,6 +4,6 @@ class ProteinDetectionHypothesis < ActiveRecord::Base
   validates :pass_threshold, :presence => true
   #has_and_belongs_to_many :peptide_evidences, :join_table => 'protein_hypothesis_pepevidence_join_table'
   belongs_to :protein_ambiguity_group
-  has_many :protein_detection_hypothesis_psi_ms_cv_terms, :dependent => destroy
-  has_many :protein_detection_hypothesis_user_params, :dependent => destroy
+  has_many :protein_detection_hypothesis_psi_ms_cv_terms, :dependent => :destroy
+  has_many :protein_detection_hypothesis_user_params, :dependent => :destroy
 end
