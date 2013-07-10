97484dc157ecb21bc4143fdd6adb647d87f86901
Vital @ UCM <uveuve@gmail.com>
2013-07-10 18:21:52 +0200

    fixed some associations and test no two modifications can occur at a certain position for a peptide

--- app/models/modification.rb
+++ app/models/modification.rb
@@ -4,4 +4,6 @@ class Modification < ActiveRecord::Base
   validates :peptide_sequence_id, :presence => true
   belongs_to :peptide_evidence
   validates :peptide_evidence_id, :presence => true
+  validates :location, :presence => true
+  validates_uniqueness_of :location, :scope => [:peptide_sequence_id, :peptide_evidence_id], :case_sensitive => false
 end
