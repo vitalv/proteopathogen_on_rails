d09e233a5eb021f2ad83bfc191b578952f06bbed
Vital @ UCM <uveuve@gmail.com>
2013-07-10 18:21:52 +0200

    fixed some associations and test no two modifications can occur at a certain position for a peptide

--- app/models/peptide_spectrum_assignment.rb
+++ app/models/peptide_spectrum_assignment.rb
@@ -3,11 +3,8 @@ class PeptideSpectrumAssignment < ActiveRecord::Base
   belongs_to :spectrum_identification_item
   validates :spectrum_identification_item_id, :presence => true
   belongs_to :peptide_evidence
-  validates :peptide_evidence_id, :presence => true
-  
-  validates_uniqueness_of :spectrum_identification_item_id, :scope => :peptide_evidence_id
-  
+  validates :peptide_evidence_id, :presence => true  
+  validates_uniqueness_of :spectrum_identification_item_id, :scope => :peptide_evidence_id  
   has_many :peptide_hypotheses
-  has_many :protein_detection_hypotheses, :through => :peptide_hypotheses
-  
+  has_many :protein_detection_hypotheses, :through => :peptide_hypotheses  
 end
--- test/unit/modification_test.rb
+++ test/unit/modification_test.rb
@@ -8,4 +8,10 @@ class ModificationTest < ActiveSupport::TestCase
   test "the truth" do 
     assert true
   end
+  test "only onte modification at a location for a pep_seq_id and pep_ev_id" do
+    mod1 = Modification.create(:location => "1", :unimod_accession => "UNIMOD:21", :peptide_evidence_id => 1, :peptide_sequence_id => 1)
+    mod2 = Modification.new(:location => "1", :unimod_accession => "UNIMOD:210", :peptide_evidence_id => 1, :peptide_sequence_id => 1)
+    assert !mod2.save, "Saving two modifications at one location of the same peptide? WTF?"
+    
+  end
 end
