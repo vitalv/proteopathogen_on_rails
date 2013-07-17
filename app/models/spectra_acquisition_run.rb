2bc2e0670be37b50910d8dfec7c7dc997ebee28e
Vital @ UCM <uveuve@gmail.com>
2013-07-17 17:26:19 +0200

    working on tables in the views for mzid_files/experiments and spectrum_identification_protocol/protocols controllers

--- app/models/spectra_acquisition_run.rb
+++ app/models/spectra_acquisition_run.rb
@@ -1,6 +1,6 @@
 class SpectraAcquisitionRun < ActiveRecord::Base
   # attr_accessible :title, :body
-  has_and_belongs_to_many :spectrum_identifications, :join_table => :sar_si_join_table, :uniq => true
+  has_and_belongs_to_many :spectrum_identifications, join_table: "sar_si_join_table"
   validates_associated :spectrum_identifications
   before_destroy { |sar| sar.spectrum_identifications.clear }
   validates :spectra_file, :uniqueness => true
