284e9a32024e25b6202cf1e36851413222e8ddd8
Vital @ UCM <uveuve@gmail.com>
2013-07-25 12:05:06 +0200

    moved spectra_files method from admin/sar controller to mzidfile model

--- app/models/mzid_file.rb
+++ app/models/mzid_file.rb
@@ -7,4 +7,31 @@ class MzidFile < ActiveRecord::Base
   belongs_to :experiment
   has_many :spectra_acquisition_runs, :dependent => :destroy
   validates_associated :spectra_acquisition_runs
+   
+  
+  def self.spectra_files  
+    @mzidf_input_spectra_files = {} #HASH: {4=>["AtiO2.mzML", "Elu1A.mzML", "Elu2A.mzML"], 5=>["MYOGLOBIN_ECD.mgf"]} 
+    #get this from existing mzidfile.spectra_acquisition_runs
+    spect_acq_runs, @stored_spectra_files = [], []  
+    self.all.each do |mzidf|
+      #get this from the mzid file
+      input_spect_files = []  
+      if File.exists? mzidf.location
+        spect_acq_runs = MzidFile.find(mzidf).spectra_acquisition_runs
+        unless spect_acq_runs.blank?
+          spect_acq_runs.each do |sar|
+            @stored_spectra_files << sar.spectra_file
+          end
+        end
+      end
+      spectra_data = Nokogiri::XML(File.open(mzidf.location)).xpath("//xmlns:SpectraData")
+      spectra_data.each do |s| #<SpectraData> minOccurs = 1
+        input_spect_files << s.attr("location").split("/")[-1] #attr location required
+      end
+      @mzidf_input_spectra_files[mzidf.id] = input_spect_files
+    end    
+    return @mzidf_input_spectra_files
+  end
+  
+  
 end
