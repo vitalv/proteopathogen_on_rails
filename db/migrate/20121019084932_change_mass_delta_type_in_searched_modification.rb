b51d9864cf52d629110a32281fdc96a2e3138f00
Vital @ UCM <uveuve@gmail.com>
2013-07-30 11:01:03 +0200

    changed mass_delta data type to string (varchar(255)) in searched_modifications

--- db/migrate/20121019084932_change_mass_delta_type_in_searched_modification.rb
+++ db/migrate/20121019084932_change_mass_delta_type_in_searched_modification.rb
@@ -1,9 +1,11 @@
 class ChangeMassDeltaTypeInSearchedModification < ActiveRecord::Migration
   def up
-    change_column :searched_modifications, :mass_delta, :decimal, :precision => 4
+    #change_column :searched_modifications, :mass_delta, :decimal, :precision => 4
+    change_column :searched_modifications, :mass_delta, :string
   end
 
   def down
-    change_column :searched_modifications, :mass_delta, :string
+    #change_column :searched_modifications, :mass_delta, :string
+    change_column :searched_modifications, :mass_delta, :decimal, :precision => 4
   end
 end
