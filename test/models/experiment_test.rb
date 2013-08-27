d9a87068fb3c608e36f07d8e56ab8371eebebe8d
Vital @ UCM <uveuve@gmail.com>
2013-08-27 09:55:41 +0200

    created some fixtures and tests, mainly to assert presence validations

--- test/models/experiment_test.rb
+++ test/models/experiment_test.rb
@@ -4,4 +4,16 @@ class ExperimentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save without organism" do
+    e = Experiment.new
+    #e.organism = "Candida"
+    assert !e.save
+  end
+  
+  test "should not save without protocol" do
+    e = Experiment.new
+    e.organism = "Candida"
+    assert !e.save
+  end
+  
 end
