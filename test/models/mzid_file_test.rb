6177a352b310c8662c372aff769bd82bcb3f3d00
Vital @ UCM <uveuve@gmail.com>
2013-08-27 09:55:41 +0200

    created some fixtures and tests, mainly to assert presence validations

--- test/models/mzid_file_test.rb
+++ test/models/mzid_file_test.rb
@@ -4,4 +4,18 @@ class MzidFileTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should not save without required attributes" do
+    mzidf = MzidFile.new
+    assert !mzidf.save
+  end
+  
 end
+
+
+
+#model
+#  validates :location, presence: true
+#  validates :sha1, uniqueness: true
+#  validates :name, presence: true
+#  validates :experiment_id, presence: true
--- test/models/protein_detection_test.rb
+++ test/models/protein_detection_test.rb
@@ -4,4 +4,8 @@ class ProteinDetectionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save protein detection without pd id" do 
+    pd = ProteinDetection.new
+    assert !pd.save
+  end
 end
