91f5e127d9d3677bf785c4416ff737a67619bf93
Vital @ UCM <uveuve@gmail.com>
2013-08-29 18:03:10 +0200

    I realized I don't want to validates presence of sip (or sil) in SI

--- app/models/spectrum_identification.rb
+++ app/models/spectrum_identification.rb
@@ -2,18 +2,26 @@ class SpectrumIdentification < ActiveRecord::Base
 
   validates :si_id, uniqueness: {scope: :mzid_file_id}, presence: true
   
-  belongs_to :mzid_files
+  belongs_to :mzid_file
   validates :mzid_file_id, presence: true
+  validates :mzid_file, presence: true
+  validates_associated :mzid_file
   
   has_and_belongs_to_many :search_databases,  join_table: "sdb_si_join_table"
   before_destroy { |si| si.search_databases.clear } #avoids orphans in join table
   
-  has_one :spectrum_identification_protocol, dependent: :destroy
-  validates_associated :spectrum_identification_protocol #<SpectrumIdentification spectrumIdentificationProtocol_ref: required > 
-  
+  has_one :spectrum_identification_protocol, dependent: :destroy   
+  #comprobar si esto hace algo!?:
+  #validates_associated :spectrum_identification_protocol #<SpectrumIdentification spectrumIdentificationProtocol_ref: required > 
+  #no, no hace nada a menos que valide su presencia antes
+   
   has_one :spectrum_identification_list, dependent: :destroy
-  validates_associated :spectrum_identification_list  #<SpectrumIdentification spectrumIdentificationList_ref: required > 
+  #validates_associated :spectrum_identification_list  #<SpectrumIdentification spectrumIdentificationList_ref: required > 
   
-  has_many :spectrum_identification_results, through: :spectrum_identification_list #, :dependent => :destroy
+  #Note: SpectrumIdentificationProtocol and SpectrumIdentificationList are both required attributes in element <SpectrumIdentification> which MUST be (and always is) present in the mzid file
+  #so I am not validating them here. 
+   
+  #~ has_many :spectrum_identification_results, through: :spectrum_identification_list #, :dependent => :destroy
+  #~ 
   
 end
