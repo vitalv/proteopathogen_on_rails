a6c24012f46adc6ab2aca22a8b0fd280e306f33d
Vital @ UCM <uveuve@gmail.com>
2013-09-13 16:30:16 +0200

    moved mzid_files_spec to spec/controllers, where it belongs

---
+++ spec/controllers/mzid_files_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+
+#this is mor a controller spec than a feature spec, you should move it to spec/controllers and use "describe" instead of "feature"
+
+describe MzidFilesController do
+ 
+  describe "GET #index " do
+    it "generates list of mzid files" do
+      mzidf = FactoryGirl.create(:mzid_file)
+      get :index
+      assigns(:all_mzid_files).should eq([mzidf]) #@all_mzid_files
+    end
+    it "displays the :index view" 
+  end
+
+end
