d0b89c6a9b530669d269113aa0c773fbff2ba978
Vital @ UCM <uveuve@gmail.com>
2013-09-17 10:54:19 +0200

    change to spectrum_identification controller spec. Test passes ok

--- spec/controllers/spectrum_identifications_spec.rb
+++ spec/controllers/spectrum_identifications_spec.rb
@@ -10,27 +10,18 @@ describe SpectrumIdentificationsController do
     end
   
     it "gets list of si_s for the corresponding experiment" do       
-      #get :index, experiment_id: mzidf.id  
       assigns(:spectrum_identifications).should eq(@mzidf.spectrum_identifications)
     end
    
     it "gets the protocol for the corresponding experiment" do
-      experiment_id = @mzidf.experiment.id
-      exp_protocol = Experiment.find(experiment_id).protocol
+      exp_protocol = Experiment.find(@mzidf.experiment.id).protocol
       assigns(:exp_protocol).should eq(exp_protocol)
     end
     
-    print page.html
     
   end
     
-    #~ mzid_file_id = params[:experiment_id]
-    #~ experiment_id = MzidFile.find(mzid_file_id).experiment_id
-    #~ @exp_protocol = Experiment.find(experiment_id).protocol
 
-    #~ @spectrum_identifications = MzidFile.find(mzid_file_id).spectrum_identifications
-  
-  
 
 end
 
