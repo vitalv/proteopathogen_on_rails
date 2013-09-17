a8051635a9ec542854486a10ba575d27467d1e6c
Vital @ UCM <uveuve@gmail.com>
2013-09-17 17:34:58 +0200

    spectrum identifications feature spec tests the si tables display correctly and links to each corresponding sir. test passes

--- app/controllers/spectrum_identification_results_controller.rb
+++ app/controllers/spectrum_identification_results_controller.rb
@@ -1,2 +1,11 @@
 class SpectrumIdentificationResultsController < ApplicationController
+
+  def index
+    si = SpectrumIdentification.find(params[:spectrum_identification_id])
+    #experiment_id = MzidFile.find(params[:experiment_id]).experiment_id
+    #@protocol = Experiment.find(experiment_id).protocol
+    #@spectra_acquisition_runs = SpectraAcquisitionRun.where(:mzid_file_id => params[:experiment_id])
+    @spectrum_identification_results = si.spectrum_identification_results
+  end
+
 end
--- spec/controllers/spectrum_identifications_spec.rb
+++ spec/controllers/spectrum_identifications_spec.rb
@@ -4,15 +4,17 @@ describe SpectrumIdentificationsController do
  
   describe "GET experiments/:experiment_id/spectrum_identifications#index " do
   
-    #~ before(:each) do    
-      #~ @mzidf = FactoryGirl.create(:mzid_file)
-      #~ get :index, experiment_id: @mzidf.id  
-    #~ end
-  #~ 
-    #~ it "gets list of si_s for the corresponding experiment" do       
-      #~ assigns(:spectrum_identifications).should eq(@mzidf.spectrum_identifications)
-    #~ end
-   #~ 
+    before(:each) do
+      @mzidf = FactoryGirl.create(:mzid_file)  
+      @mzidf.spectrum_identifications = [FactoryGirl.create(:spectrum_identification)]
+      get :index, mzid_file_id: @mzidf.id  
+    end
+   
+    it "gets list of si_s for the corresponding mzid file" do
+      @mzidf.spectrum_identifications.should_not be_empty
+      assigns(:spectrum_identifications).should eq(@mzidf.spectrum_identifications)
+    end
+    
     #~ it "gets the protocol for the corresponding experiment" do
       #~ exp_protocol = Experiment.find(@mzidf.experiment.id).protocol
       #~ assigns(:exp_protocol).should eq(exp_protocol)
