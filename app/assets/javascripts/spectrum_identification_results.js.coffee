c4f7541ab678a3e90b947c1c4bb75bca63e64df2
Vital @ UCM <uveuve@gmail.com>
2013-09-18 16:01:38 +0200

    spectrum identification results view

--- app/assets/javascripts/spectrum_identification_results.js.coffee
+++ app/assets/javascripts/spectrum_identification_results.js.coffee
@@ -1,3 +1,12 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$ ->
+  $("tr[data-sir-id]").click ( ->
+    window.location = $(this).find('a').attr('href')
+  ).hover -> 
+    $(this).toggleClass "hover"
+    
+  
--- spec/controllers/spectrum_identification_results_spec.rb
+++ spec/controllers/spectrum_identification_results_spec.rb
@@ -1,28 +1,25 @@
 require 'spec_helper'
 
-describe SpectrumIdentificationsController do
+describe SpectrumIdentificationResultsController do
  
   describe "GET spectrum_identifications/:spectrum_identification_id/spectrum_identification_results#index " do
   
-    #before(:each) do    
-    #  @mzidf = FactoryGirl.create(:mzid_file)
-    #  get :index, experiment_id: @mzidf.id  
-    #end
-   si = FactoryGirl.create(:spectrum_identification)
-   get :index, spectrum_identification_id: si.id
+    before "given I have a sil for the si, and sir_s for the sil" do
+    #- (Should I only save sil for already saved sir_s and si for already saved sil_s and so on... ???
+      @si = FactoryGirl.create(:spectrum_identification)
+      @si.spectrum_identification_list = FactoryGirl.create(:spectrum_identification_list)
+      sil = @si.spectrum_identification_list
+      @si.spectrum_identification_list.spectrum_identification_results = [FactoryGirl.create(:spectrum_identification_result)]
+      @sirs = sil.spectrum_identification_results
+      get :index, spectrum_identification_id: @si.id
+    end
+    
+    it "generates list of sir_s for the corresponding  si" do
+      assigns(:spectrum_identification_results).should eq(@sirs)
+    end
    
-    #~ it "gets list of si_s for the corresponding experiment" do       
-      #~ assigns(:spectrum_identifications).should eq(@mzidf.spectrum_identifications)
-    #~ end
-   #~ 
-    #~ it "gets the protocol for the corresponding experiment" do
-      #~ exp_protocol = Experiment.find(@mzidf.experiment.id).protocol
-      #~ assigns(:exp_protocol).should eq(exp_protocol)
-    #~ end
-    #~ 
     
-  end
-    
+  end    
 
 
 end
