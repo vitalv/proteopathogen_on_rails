e7eacd8df1ac6d0d98b3655d03f7faae5137c56f
Vital @ UCM <uveuve@gmail.com>
2014-01-10 12:01:42 +0100

    moved the d3 spectrum thing, js and css, from experiments to spectrum_identification_resutls

--- app/controllers/spectrum_identification_results_controller.rb
+++ app/controllers/spectrum_identification_results_controller.rb
@@ -29,10 +29,11 @@ class SpectrumIdentificationResultsController < ApplicationController
   
   def identification_item
     @sii = params[:sii_id]
-    
+    @jsonFragmentIons = SpectrumIdentificationItem.find(@sii).fragments.as_json
     respond_to do |format|
-      format.json {}
-      format.html {render partial: 'spectrum'}
+      format.html 
+      format.json { }
+      format.js { render  :layout => false } 
     end
   end
 
