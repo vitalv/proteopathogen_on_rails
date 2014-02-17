d35eb3b0e79a23aa4cc4e695da7d5e621f91ca8a
Vital @ UCM <uveuve@gmail.com>
2014-02-17 16:04:41 +0100

    lots of changes including the info icon thing to show info from mzidentml doc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   
   def index
+    
+    @experiment_count = Experiment.all.count
+    @mzid_file_count = MzidFile.all.count
+    @peptide_evidence_count = PeptideEvidence.all.count
+    @peptide_sequence_count = PeptideSequence.all.count
+    @psm_count = PeptideSpectrumAssignment.all.count
+    @pdh_count = ProteinDetectionHypothesis.all.count
   end
 
 end
