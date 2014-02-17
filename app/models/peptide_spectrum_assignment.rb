310e505b6ff3b5e6092a900b13d23fde639c7269
Vital @ UCM <uveuve@gmail.com>
2014-02-17 19:02:39 +0100

    added some stuff to the home view

--- app/models/peptide_spectrum_assignment.rb
+++ app/models/peptide_spectrum_assignment.rb
@@ -2,7 +2,7 @@ class PeptideSpectrumAssignment < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :spectrum_identification_item
   validates :spectrum_identification_item_id, uniqueness: {scope: :peptide_evidence_id}, presence: true
-  belongs_to :peptide_evidence
+  belongs_to :peptide_evidence, dependent: :destroy
   validates :peptide_evidence_id, uniqueness: {scope: :spectrum_identification_item_id}, presence: true  
   has_many :peptide_hypotheses, dependent: :destroy
   has_many :protein_detection_hypotheses, through: :peptide_hypotheses  
