3c0e8f101237c7dc36441b8dbdd77c915113a130
Vital Vialas <uveuve@gmail.com>
2014-02-20 19:08:18 +0100

    changes to search database model and saving

--- app/models/search_database.rb
+++ app/models/search_database.rb
@@ -3,6 +3,8 @@ class SearchDatabase < ActiveRecord::Base
   has_and_belongs_to_many :spectrum_identifications, :join_table => :sip_sdb_join_table
   before_destroy { |sdb| sdb.spectrum_identifications.clear } #avoids orphans in join table
   
+  #self.location = "/unkown/location" if self.location == nil or self.location == ""
+  
   validates :location, presence: true #attribute location is required in <SearchDatabase> in the mzid file
   
   has_many :db_sequences, dependent: :destroy
