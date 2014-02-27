01cb56e6753f66dbc707ee3639e6ebd75d14118c
Vital @ UCM <uveuve@gmail.com>
2014-02-27 15:33:57 +0100

    fixed the tables showing mzid files to multiple rows instead of multipple inner tables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,13 @@ class HomeController < ApplicationController
     @peptide_sequence_count = PeptideSequence.all.count
     @psm_count = PeptideSpectrumAssignment.all.count
     @pdh_count = ProteinDetectionHypothesis.all.count
+    #To get a number of proteins I can get:
+    #Accessions from DbSequence
+    @dbseq_accession_count = DbSequence.all.map { |d| d.accession}
+    @distinct_dbseq_accession_count = DbSequence.all.map { |d| d.accession}.uniq.count #remember DbSeq accession is scoped to sdb_id in DbSeq model so I need the ".uniq" to get proteins are considered different 
+    #Actual DbSequence (protein) sequences
+    @dbseq_sequence_count = DbSequence.all.map { |d| d.sequence}.count
+    
   end
 
 end
